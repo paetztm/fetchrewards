49a1f5a00aef698ed2c6c3819b56385876d27d75
paetztm <paetztm@gmail.com>
2025-04-17 12:31:55 -0700

    Added branding

--- app/src/main/java/com/timothypaetz/android/fetchrewards/ui/theme/Color.kt
+++ app/src/main/java/com/timothypaetz/android/fetchrewards/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryColor = Color(0xFF390754)
+val ErrorHeaderColor = Color(0xFF3C3353)
+val BackgroundGray = Color(0xFFF3F3F3)
